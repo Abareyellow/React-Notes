97f683f9373795ed86b0c8582935c2f4dbea9884
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-11-12 00:14:42 +0000

    Finished for today!!!

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -3,12 +3,24 @@ import './App.css'
 //let language = "Katie";
 //let sun = "🌞";
 
-function Header(props) {
+let items = ["Brownies", "Cakes", "Cookies"]
+
+function Header({name, year}) {
   return (
     <header>
-      <h1>{props.name}'s Kitchen</h1>
-      <p>Copyright {props.year}</p>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
     </header>
+  );
+}
+
+function Main({menu}) {
+  return (
+    <ul>
+      {menu.map((dish) => (
+        <li>{dish}</li>
+      ))}
+    </ul>
   );
 }
 
@@ -16,9 +28,7 @@ function App() {
   return (
     <div>
       <Header name="Katie" year={2024}/>
-      <main>
-        <h2>We serve the most delicious food around!</h2>
-      </main>
+      <Main menu={items}/>
     </div>
   );
 }
