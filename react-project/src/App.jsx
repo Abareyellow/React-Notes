446f77ec23ec72705504b6920f54f00ea28587e4
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-12-03 02:30:58 +0000

    learned some react!!!

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from "react";
 import './App.css'
 import chef from "./images/chef.jpg"
 
@@ -11,8 +13,6 @@ const dishObjects = items.map((dish, i) => ({
   title: dish
 }));
 
-console.log(dishObjects)
-
 function Header({name, year}) {
   return (
     <header>
@@ -24,20 +24,28 @@ function Header({name, year}) {
 
 function Main({menu}) {
   return (
-    <main>
-      <img src={chef} height={200} alt="A photo of a chef"/>
-      <ul>
-        {menu.map((dish) => (
-          <li key={dish.id} style={{listStyleType: "none"}}>{dish.title}</li>
-        ))}
-      </ul>
-    </main>
+    <>
+      <div>
+        <h2>Welcome to this beautiful resturant!!</h2>
+      </div>
+      <main>
+        <img src={chef} height={200} alt="A photo of a chef"/>
+        <ul>
+          {menu.map((dish) => (
+            <li key={dish.id} style={{listStyleType: "none"}}>{dish.title}</li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
 function App() {
+  const [status, setStatus] = useState(true);
   return (
     <div>
+      <h1>The resturant is currently {status ? "open" : "closed"}.</h1>
+      <button onClick={() => setStatus(!status)}>{status ? "Closed" : "Open"} Resturant</button>
       <Header name="Katie" year={2024}/>
       <Main menu={dishObjects}/>
     </div>
